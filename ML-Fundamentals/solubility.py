3aeb56eea8976468466f55f30896af1007dfed34
FelipeDuranl <felipeduram123@gmail.com>
2026-03-11 22:48:13 -0300

    First learn

---
+++ ML-Fundamentals/solubility.py
@@ -0,0 +1,34 @@
+from sklearn.svm import LinearSVC
+from sklearn.metrics import accuracy_score
+
+composto1 = [1, 1, 1]
+composto2 = [0, 0, 0] 
+composto3 = [1, 0, 0] 
+composto4 = [0, 1, 0] 
+composto5 = [1, 1, 0] 
+composto6 = [0, 0, 1]
+
+dados_treino = [composto1, composto2, composto3, composto4, composto5, composto6]
+rotulos_treino = ['S', 'N', 'S', 'N', 'S', 'S']
+
+modelo = LinearSVC()
+modelo.fit(dados_treino, rotulos_treino)
+
+teste1 = [1, 0, 0]
+teste2 = [0, 1, 1]
+teste3 = [1, 0 ,1]
+
+dados_teste = [teste1, teste2, teste3]
+rotulos_teste = ['S', 'S', 'S']
+
+previsoes = modelo.predict(dados_teste)
+
+mapeamento_previsoes = {'S': 'Soluvel', 'N': 'Não Soluvel'}
+
+taxa_acerto = accuracy_score(rotulos_teste, previsoes)
+
+print ("Previsões do modelo para os compostos testados:", previsoes)
+for i, previsao in enumerate(previsoes):
+    print(f'O composto {i+1} pode ser considerado {mapeamento_previsoes[previsao]}')
+
+print("taxa de acerto: %.2f%%" % (taxa_acerto * 100))
